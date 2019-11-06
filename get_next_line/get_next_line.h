96d6aac8aabf3667995072411e124ce3f415d60a
Ch_BSoD <hamza.chorfi.des@gmail.com>
2019-11-06 17:28:36 +0100

    GNL succeeded after 3 pushs.
    the problem was in the last return. i should free the static pointer before return 0

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchorfi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/30 19:48:15 by hchorfi           #+#    #+#             */
+/*   Updated: 2019/10/31 22:45:58 by hchorfi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define M 2147483647
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+char	*ft_strdup(const char *s1);
+int		ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+void	ft_bzero(void *s, size_t n);
+int		get_next_line(int fd, char **line);
+
+#endif
