3c9c5c2023820ca37c772ba1849004c1b9a7f86b
Hamza Chorfi <hchorfi@e1r13p8.1337.ma>
2020-11-25 13:57:33 +0100

    test

---
+++ ft_services/setup.sh
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    setup.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: hchorfi <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/11/24 13:27:51 by hchorfi           #+#    #+#              #
+#    Updated: 2020/11/24 13:27:52 by hchorfi          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/sh
+
+### starting minikube
+echo "/-----------------------------------------------------------------------/"
+echo "/starting minikube"
+echo "/-----------------------------------------------------------------------/"
+export MINIKUBE_HOME=/Volumes/HAKUNA_MATA
+minikube start --driver virtualbox
+minikubeip=$(minikube ip)
+echo $minikubeip
+sed -i '' "s/192.168.99.*/$minikubeip-$minikubeip/g" ./srcs/metallb-configmap.yaml
+
+### deploy MetalLB
+echo "/-----------------------------------------------------------------------/"
+echo "/deploy MetalLB"
+echo "/-----------------------------------------------------------------------/"
+#kubectl apply -f \
+#https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+#kubectl apply -f \
+#https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+#kubectl create secret generic -n metallb-system memberlist \
+#--from-literal=secretkey="$(openssl rand -base64 128)"
+
+
