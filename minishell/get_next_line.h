c52992902495eff0021595a489e33e26d825063d
hchorfi <hamza.chorfi.des@gmail.com>
2021-02-04 14:12:15 +0100

    first loop

--- minishell/get_next_line.h
+++ minishell/get_next_line.h
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hchorfi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hchorfi <hchorfi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 19:48:15 by hchorfi           #+#    #+#             */
-/*   Updated: 2019/10/31 22:45:58 by hchorfi          ###   ########.fr       */
+/*   Updated: 2021/02/04 00:06:35 by hchorfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define M 2147483647
+# define BUFFER_SIZE 64
 
 # include <fcntl.h>
 # include <unistd.h>
--- minishell/main.c
+++ minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: hchorfi <hchorfi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 22:39:14 by devza             #+#    #+#             */
-/*   Updated: 2021/01/22 23:01:58 by hchorfi          ###   ########.fr       */
+/*   Updated: 2021/02/04 00:05:43 by hchorfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 int main()
 {
     char *line;
-    write(1, "minishell 👽 > ", 18);
-	get_next_line(0, &line);
-    printf("you need to pars this 💩 : |%s|\n", line);
+    while (1)
+    {
+        write(1, "minishell 👽 > ", 18);
+	    get_next_line(0, &line);
+        if (line[0] == '*')
+            exit (0);
+        printf("you need to pars this 💩 : |%s|\n", line);
+    }
 }
