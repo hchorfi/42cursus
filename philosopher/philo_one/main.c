fa8fc520b9e9e20b68718344488da0372d218b61
hchorfi <hamza.chorfi.des@gmail.com>
2021-05-19 12:43:19 +0100

    philo one threads

--- philosopher/philo_one/main.c
+++ philosopher/philo_one/main.c
@@ -6,11 +6,14 @@
 /*   By: hchorfi <hchorfi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 14:52:14 by hchorfi           #+#    #+#             */
-/*   Updated: 2021/05/18 19:51:34 by hchorfi          ###   ########.fr       */
+/*   Updated: 2021/05/18 22:57:31 by hchorfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
+
+pthread_mutex_t mutex;
+int mail = 0;
 
 void	ft_init_philos(int argc, char **argv, t_data *data, t_ph_data *ph_data)
 {
@@ -34,11 +37,63 @@ void	ft_init_philos(int argc, char **argv, t_data *data, t_ph_data *ph_data)
 
 void	ft_print_philo_data(t_ph_data *ph_data, t_data *data)
 {
-	while (ph_data)
+	int i;
+
+	i = 0;
+	printf("%d philosopher on table\n", data->n_philos);
+	if (data->max_ph_eat != -1)
+		printf("each philosopher should eat max %d times\n", data->max_ph_eat);
+	printf("**************\n");
+	while (i < data->n_philos)
 	{
-		printf("ok\n");
-		ph_data++;
+		printf("%d philo number %d \n", i, ph_data[i].ph_number);
+		printf("----------------\n");
+		printf("time_to_die : %d\n", ph_data[i].tt_die);
+		printf("time_to_eat : %d\n", ph_data[i].tt_eat);
+		printf("time_to_sleep : %d\n\n", ph_data[i].tt_sleep);
+		i++;
 	}
+	
+}
+
+void	ft_clear(t_ph_data *ph_data, pthread_t *thread)
+{
+	free(ph_data);
+	free(thread);
+}
+
+void	*ft_routine()
+{
+	int j;
+	j = 0;
+	while (j < 10000000)
+	{
+		pthread_mutex_lock(&mutex);
+		mail++;
+		pthread_mutex_unlock(&mutex);
+		j++;
+	}
+}
+
+void	ft_thread(pthread_t *thread, t_data *data)
+{
+	int i;
+
+	i = 0;
+	while (i < data->n_philos)
+	{
+		pthread_create(&thread[i], NULL, &ft_routine, NULL);
+		printf("thread %d created\n", i);
+		i++;
+	}
+	i = 0;
+	while (i < data->n_philos)
+	{
+		pthread_join(thread[i], NULL);
+		printf("thread %d finish his job\n", i);		
+		i++;
+	}
+	
 	
 }
 
@@ -46,12 +101,19 @@ int main(int argc, char **argv)
 {
 	t_data		data;
 	t_ph_data	*ph_data;
+	pthread_t	*thread;
 
 	if (argc == 5 || argc == 6)
 	{
+		pthread_mutex_init(&mutex, NULL);
 		ph_data = malloc(sizeof(t_ph_data) * ft_atoi(argv[1]));
+		thread = malloc(sizeof(pthread_t) * ft_atoi(argv[1]));
 		ft_init_philos(argc, argv, &data, ph_data);
-		ft_print_philo_data(ph_data, &data);
+		ft_thread(thread, &data);
+		//ft_print_philo_data(ph_data, &data);
+		ft_clear(ph_data, thread);
+		pthread_mutex_destroy(&mutex);
+		printf("%d\n", mail);
 	}
 	else
 		return (arg_count_err());
